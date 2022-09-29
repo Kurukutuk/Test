9c2e98a4bb7abe7858ff70074f6badc5ede282f1
Kurukutuk <rangiefamilargas@gmail.com>
2022-09-29 17:12:45 +0800

    Commit 2

--- src/main/java/com/test/PaginationSorting/controller/UserController.java
+++ src/main/java/com/test/PaginationSorting/controller/UserController.java
@@ -3,5 +3,6 @@ package com.test.PaginationSorting.controller;
 public class UserController {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Hi");
     }
 }
